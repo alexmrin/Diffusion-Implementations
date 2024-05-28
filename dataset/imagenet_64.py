ce7ddbff75ccdbf1b450f4d76bfdd706bf598999
Alex <118281736+alexmrin@users.noreply.github.com>
2024-05-28 22:53:47 +0900

    updated imagenet

--- dataset/imagenet_64.py
+++ dataset/imagenet_64.py
@@ -1,28 +1,49 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset, Subset
 from torchvision import transforms
 from datasets import load_dataset
+from torch.utils.data.dataloader import default_collate
+
+class ImagenetDataset(Dataset):
+    def __init__(self, dataset_name, split, transform=None):
+        self.dataset = load_dataset(dataset_name, split=split)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        image = item['image']
+        label = item['label']
+
+        # Apply the transform to the image if one is provided
+        if self.transform:
+            image = self.transform(image)
+        
+        # Check if the image has the desired size [3, 64, 64]
+        if image.size() == torch.Size([3, 64, 64]):
+            return image, label
+        else:
+            return None
+        
+def custom_collate_fn(batch):
+    batch = list(filter(lambda x: x is not None, batch))
+    return torch.utils.data.dataloader.default_collate(batch)
 
 def prepare_imagenet64_loader(batch_size=32):
     train_transform = transforms.Compose([
-        transforms.Resize((64, 64)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
     ])
     test_transform = transforms.Compose([
-        transforms.Resize((64, 64)),
         transforms.ToTensor(),
     ])
-    def transform_features(example, transform):
-        example['image'] = transform(example['image'])
-        return example
-
-    def load_dataset_split(split, transform):
-        dataset = load_dataset("zh-plus/tiny-imagenet", split=split)
-        dataset = dataset.map(lambda x: transform_features(x, transform), remove_columns=['image'], load_from_cache_file=False)
-        return DataLoader(dataset, batch_size=batch_size, shuffle=(split == 'train'), num_workers=6, drop_last=True)
-    
-    train_loader = load_dataset_split('train', train_transform)
-    validation_loader = load_dataset_split('validation', test_transform)
-    
+    train_data = ImagenetDataset('zh-plus/tiny-imagenet', 'train', train_transform)
+    test_data = ImagenetDataset('zh-plus/tiny-imagenet', 'valid', test_transform)
+    test_size = len(test_data) // 10
+    indices = torch.randperm(len(test_data)).tolist()
+    test_subset = Subset(test_data, indices[:test_size])
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4, drop_last=True, collate_fn=custom_collate_fn)
+    validation_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False, num_workers=4, drop_last=True, collate_fn=custom_collate_fn)
     return train_loader, validation_loader
